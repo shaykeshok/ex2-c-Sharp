0d00c864d6ec7b87e15d12142fc432c436a57d52
shaykeshok <shok.shayke@gmail.com>
2020-05-02 21:20:54 +0300

    Add project files.

---
+++ BFS/BFS.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    class BFS
+    {
+        public class Graph<T>
+        {
+            public Graph() { }
+            public Graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges)
+            {
+                foreach (var vertex in vertices)
+                    AddVertex(vertex);
+
+                foreach (var edge in edges)
+                    AddEdge(edge);
+            }
+
+            public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>>();
+
+            public void AddVertex(T vertex)
+            {
+                AdjacencyList[vertex] = new HashSet<T>();
+            }
+
+            public void AddEdge(Tuple<T, T> edge)
+            {
+                if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
+                {
+                    AdjacencyList[edge.Item1].Add(edge.Item2);
+                    AdjacencyList[edge.Item2].Add(edge.Item1);
+                }
+            }
+        }
+        public class Algorithms
+        {
+            public HashSet<T> BFS<T>(Graph<T> graph, T start)
+            {
+                var visited = new HashSet<T>();
+
+                if (!graph.AdjacencyList.ContainsKey(start))
+                    return visited;
+
+                var queue = new Queue<T>();
+                queue.Enqueue(start);
+
+                while (queue.Count > 0)
+                {
+                    var vertex = queue.Dequeue();
+
+                    if (visited.Contains(vertex))
+                        continue;
+
+                    visited.Add(vertex);
+
+                    foreach (var neighbor in graph.AdjacencyList[vertex])
+                        if (!visited.Contains(neighbor))
+                            queue.Enqueue(neighbor);
+                }
+
+                return visited;
+            }
+        }
+    }
+}
---
+++ git/BFS/Hangman.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Hangman
+    {
+       
+
+        //    Console.WriteLine("Welcome to Hangman!!!!!!!!!!");
+        //    string[] listwords = new string[10];
+        //    listwords[0] = "sheep";
+        //    listwords[1] = "goat";
+        //    listwords[2] = "computer";
+        //    listwords[3] = "america";
+        //    listwords[4] = "watermelon";
+        //    listwords[5] = "icecream";
+        //    listwords[6] = "jasmine";
+        //    listwords[7] = "pineapple";
+        //    listwords[8] = "orange";
+        //    listwords[9] = "mango";
+        //    Random randGen = new Random();
+        //    var idx = randGen.Next(0, 9);
+        //    string mysteryWord = listwords[idx];
+        //    char[] guess = new char[mysteryWord.Length];
+        //    Console.Write("Please enter your guess: ");
+
+        //    for (int p = 0; p < mysteryWord.Length; p++)
+        //        guess[p] = '*';
+
+        //    while (true)
+        //    {
+        //        string playerGuessStr = (Console.ReadLine());
+        //        Console.Write("Please enter your guess2: ");
+        //        char playerGuess = char.Parse(Console.ReadLine());
+        //        for (int j = 0; j < mysteryWord.Length; j++)
+        //        {
+        //            if (playerGuess == mysteryWord[j])
+        //                guess[j] = playerGuess;
+        //        }
+        //        Console.WriteLine(guess);
+        //    }
+        //}
+
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Let's play Hangman!");
+
+            Console.WriteLine("Enter a word: ");
+            string secretWord = Console.ReadLine();
+
+            //test to make sure only letters
+            bool wordTest = secretWord.All(Char.IsLetter);
+
+            while (wordTest == false || secretWord.Length == 0)
+            {
+                Console.WriteLine("A word must contain (only) letters");
+                Console.Write("Please enter a word: ");
+                secretWord = Console.ReadLine();
+                wordTest = secretWord.All(Char.IsLetter);
+            }
+
+
+            secretWord = secretWord.ToUpper();
+
+            //Console.Clear not working on Repl.it
+            //Using this to hide the secret word
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+                              "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+                              "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+            int lives = 5;
+            int counter = -1;
+            int wordLength = secretWord.Length;
+            char[] secretArray = secretWord.ToCharArray();
+            char[] printArray = new char[wordLength];
+            char[] guessedLetters = new char[26];
+            int numberStore = 0;
+            bool victory = false;
+
+            foreach (char letter in printArray)
+            {
+                counter++;
+                printArray[counter] = '-';
+            }
+
+            while (lives > 0)
+            {
+                counter = -1;
+                string printProgress = String.Concat(printArray);
+                bool letterFound = false;
+                int multiples = 0;
+
+                if (printProgress == secretWord)
+                {
+                    victory = true;
+                    break;
+                }
+
+                if (lives > 1)
+                {
+                    Console.WriteLine("You have {0} lives!", lives);
+                }
+                else
+                {
+                    Console.WriteLine("You only have {0} life left!!", lives);
+                }
+
+
+                Console.WriteLine("current progress: " + printProgress);
+                Console.Write("\n\n\n");
+                Console.Write("Guess a letter: ");
+                string playerGuess = Console.ReadLine();
+
+                //test to make sure a single letter
+                bool guessTest = playerGuess.All(Char.IsLetter);
+
+                while (guessTest == false || playerGuess.Length != 1)
+                {
+                    Console.WriteLine("Please enter only a single letter!");
+                    Console.Write("Guess a letter: ");
+                    playerGuess = Console.ReadLine();
+                    guessTest = playerGuess.All(Char.IsLetter);
+                }
+
+                playerGuess = playerGuess.ToUpper();
+                char playerChar = Convert.ToChar(playerGuess);
+
+                if (guessedLetters.Contains(playerChar) == false)
+                {
+
+                    guessedLetters[numberStore] = playerChar;
+                    numberStore++;
+
+                    foreach (char letter in secretArray)
+                    {
+                        counter++;
+                        if (letter == playerChar)
+                        {
+                            printArray[counter] = playerChar;
+                            letterFound = true;
+                            multiples++;
+                        }
+
+                    }
+
+                    if (letterFound)
+                    {
+                        Console.WriteLine("Found {0} letter {1}!", multiples, playerChar);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No letter {0}!", playerChar);
+                        lives--;
+                    }
+                    Console.WriteLine(GallowView(lives));
+                }
+                else
+                {
+                    Console.WriteLine("You already guessed {0}!!", playerChar);
+                }
+
+
+            }
+
+
+            if (victory)
+            {
+                Console.WriteLine("\n\nThe word was: {0}", secretWord);
+                Console.WriteLine("\n\nYOU WIN!!!!!!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("\n\nThe word was: {0}", secretWord);
+                Console.WriteLine("\n\nYOU LOSE!!!!!!!!!");
+            }
+
+        }
+
+        private static string GallowView(int livesLeft)
+        {
+            //simple function to print out the hangman
+
+            string drawHangman = "";
+
+            if (livesLeft < 5)
+            {
+                drawHangman += "--------\n";
+            }
+
+            if (livesLeft < 4)
+            {
+                drawHangman += "       |\n";
+            }
+
+            if (livesLeft < 3)
+            {
+                drawHangman += "       O\n";
+            }
+
+            if (livesLeft < 2)
+            {
+                drawHangman += "      /|\\ \n";
+            }
+            if (livesLeft == 0)
+            {
+                drawHangman += "      / \\ \n";
+            }
+
+            return drawHangman;
+
+        }
+
+
+
+
+    }
+}
+
